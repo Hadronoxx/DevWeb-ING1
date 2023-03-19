a8b96b9d634ebee8e41a6d8556d3852529263e53
mamertoo <83581097+mamertoo@users.noreply.github.com>
2023-03-19 18:13:30 +0100

    Add files via upload

--- accueil.js
+++ accueil.js
@@ -91,19 +91,19 @@ function initStock(){
 
 
 
-function ajouter1(borne){
-  elt=document.getElementById(borne).previousElementSibling;
+function ajouter1(obj){
+  elt=obj.previousElementSibling;
   console.log(elt);
   var valeur=parseInt(elt.value,10);
-  if(valeur<document.getElementById(borne).parentNode.previousElementSibling.innerHTML){
+  if(valeur<elt.parentNode.previousElementSibling.innerHTML){
     valeur+=1;
     elt.value=valeur;
   }
   
 }
 
-function enlever1(borne){
-  elt=document.getElementById(borne).nextElementSibling;
+function enlever1(obj){
+  elt=obj.nextElementSibling;
   var valeur=parseInt(elt.value,10);
   if (valeur>0){
     valeur-=1;
@@ -129,7 +129,7 @@ function verifierpanier(obj){
   console.log(obj.previousElementSibling.previousElementSibling);
   console.log("StockMaxi="+stockMaxi+"    Valeur="+valeur);
   if (valeur<0 || valeur> stockMaxi){
-    alert("Erreur, vous devez saisir une valeur comprise entre 0 et "+stockMaxi);
+    alert("Erreur, vous devez saisir une valeur comprise entre 1 et "+stockMaxi);
   }else{
     var res=stockMaxi-valeur;  
     obj.parentNode.previousElementSibling.innerHTML=res;
@@ -139,5 +139,33 @@ function verifierpanier(obj){
 }
 
 function envoiMessage(){
-  window.location.href="mailto:adresse@email.com"+encodeURIComponent("test")+encodeURIComponent("Message");
+  let email=encodeURIComponent("diegoojedachercoles@gmail.com");
+  let radios =document.getElementsByName("genre");
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      var sexe=radios[i].value;
+      //console.log("Sexe selectionné:"+ radios[i].value);
+      break;
+    }
+
+  }
+var selectElement = document.getElementById("selection");
+var selection = selectElement.options[selectElement.selectedIndex].textContent;
+//console.log("Selection: "+selection);
+
+  var message= "Nom: "+document.getElementById("nom").value
+  +"\nPrénom: "+document.getElementById("prenom").value+
+  "\nEmail: "+document.getElementById("email").value +
+  "\nSexe: "+sexe+
+  "\nDate de naissance"+document.getElementById("datenaissance").value;
+  "\nMétier: "+selection;
+
+
+  var sujet=document.getElementById("sujet").value;
+
+  message= encodeURIComponent(message);
+  window.location.href="mailto:"+email+"?subject="+sujet+"&body="+message;
+
+
+ /*mailto:adresse-email@domaine.com?subject=Sujet de l'email&body=Corps de l'email&cc=adresse-email2@domaine.com"*/
 }
